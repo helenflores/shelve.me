5e25491c90cb02633b323373a14d778476d13498
Helen <10841297@unitec.edu>
2010-11-18 07:48:35 -0600

    El model Review ya funciona

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,4 +3,6 @@ class Book < ActiveRecord::Base
 
       validates_presence_of :title, :author
       validates_uniqueness_of :title
+
+	has_many :reviews
 end
---
+++ app/models/review.rb
@@ -0,0 +1,15 @@
+class Review < ActiveRecord::Base
+	belongs_to :user
+	belongs_to :book
+
+	validate_presence_of :review
+	validate_length_of :review, :maximum => 400
+
+	before_salve :default_quality
+
+QUALITY_OPTIONS = ["Bad", "Regular", "Good"]
+DEFAULT_QUALITY = QUALITY_OPTIONS[1]
+	def default_quality
+		self.quality = DEFAULT_QUALITY if self.quality.blank?
+	end
+end
